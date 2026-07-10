4ab2d076b8c8fe494cf186824860cb3d5fddef2e
BUHLE <buhlentombikhoza@gmail.com>
2026-07-10 05:44:38 +0200

    Worked on making the display notes function work, added the notes html code into j.s, the added notes from the site  will be added on the html notes node

--- google-keep/googlekeep.js
+++ google-keep/googlekeep.js
@@ -1,5 +1,5 @@
 class Note {
-    constructor(title, text){
+    constructor(id, title, text){
         this.id = id;
         this.title = title;
         this.text = text;
@@ -12,8 +12,9 @@ class App {
 
         this.$activeForm = document.querySelector(".active-form");
         this.$inactiveForm = document.querySelector(".inactive-form");
-        this.$noteTitle = document.querySelector(".note-title");
-        this.$noteText = document.querySelector(".note-text");
+        this.$noteTitle = document.querySelector("#note-title");
+        this.$noteText = document.querySelector("#note-text");
+        this.$notes = document.querySelector(".notes")
 
         this.addEventListeners();
     }
@@ -29,12 +30,13 @@ class App {
         const isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
         const title = this.$noteTitle.value;
         const text = this.$noteText.value;
-        console.log(cuid())
+        
 
         if(isInactiveFormClickedOn) {
             this.openActiveForm();
         }
         else if(!isInactiveFormClickedOn && !isActiveFormClickedOn){
+            this.addNote({title, text});
             this.closeActiveForm();
         }
     }
@@ -48,11 +50,17 @@ class App {
     closeActiveForm(){
         this.$inactiveForm.style.display = "block";
         this.$activeForm.style.display = "none";
+        this.$noteText.value = "";
+        this.$noteTitle.value = "";
     }
 
-    addNote(id, { title, text}){
-        const newNote = new Note(id, title, text);
-        this.notes = [...this.notes, newNote];
+    addNote({ title, text}){
+        if (text !="") {
+        
+            const newNote = new Note(cuid(),title, text);
+            this.notes = [...this.notes, newNote];
+            this.displayNotes();
+        }
     }
 
     editNote(id, {title, text}) {
@@ -62,23 +70,62 @@ class App {
                 note.text = text;
             }
             return note;
-        })
+        });
 
 
     }
 
     displayNotes(){
-        this.notes.map(note => console.log(`
-            ID: ${note.id}
-            Title: ${note.title}
-            Text: ${note.text}
-            `))
+        this.$notes.innerHTML = this.notes.map((note) =>
+            `
+                <div class="note" id="$(note.id)">
+                    <span class="material-symbols-outlined check-circle">check_circle</span>
+                    <div class="title">${note.title}</div>
+                    <div class="text">${note.text}</div>
 
+                    <div class="note-footer">
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">palette</span>
+                            <span class="tooltip-text">Background options</span>
+
+                        </div>
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">add_alert</span>
+                            <span class="tooltip-text">Remind me</span>
+
+                        </div>
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">person_add</span>
+                            <span class="tooltip-text">Collaborator</span>
+
+                        </div>
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">image</span>
+                            <span class="tooltip-text">Add image</span>
+
+                        </div>
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">archive</span>
+                            <span class="tooltip-text">Archive</span>
+
+                        </div>
+
+                        <div class="tooltip">
+                            <span class="material-symbols-outlined hover small-icon">more_vert</span>
+                            <span class="tooltip-text">More</span>
+                        </div>                    
+                    </div>
+                </div>
+            `).join("");
     }
 
-    deleteNote({}){
+    deleteNote(id){
         this.notes = this.notes.filter((note) => note.id != id);
-
     } 
 }
 
